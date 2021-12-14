919de346c4ab5e3b310f629f1e1b938a2c5db3e8
unknown <su.userax@gmail.com>
2021-12-14 19:56:19 +0545

    server fix

--- server/Server.js
+++ server/Server.js
@@ -14,6 +14,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicDirectory, 'index.html'));
 });
 
-server.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("server is up on");
 });
