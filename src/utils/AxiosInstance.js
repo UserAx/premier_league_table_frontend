a95c4409b20ce971decf1f068a4fdd4437528cf3
unknown <su.userax@gmail.com>
2021-12-14 20:46:19 +0545

    server fix

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export default (BASEURL) => {
     const axiosInstance = axios.create({
         baseURL: BASEURL,
-        headers: {
-            'Access-Control-Allow-Origin': '*', 
-            // Accept: 'application/json',
-            // 'Content-Type': 'application/json',
-          },
+        // headers: {
+        //     // 'Access-Control-Allow-Origin': '*', 
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        //   },
     });
     return axiosInstance;
 }
